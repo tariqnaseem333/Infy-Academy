dc177807141ec5c7d9e28cb2925ec1ec3e222a85
Tariq Naseem <tariqnaseem333@gmail.com>
2023-11-24 17:27:38 +0530

    Implemented Validator Class which validates the user Input

--- Infy_Academy/src/main/java/com/infy/userinterface/CandidateTester.java
+++ Infy_Academy/src/main/java/com/infy/userinterface/CandidateTester.java
@@ -2,7 +2,7 @@ package com.infy.userinterface;
 
 import java.time.LocalDate;
 import com.infy.model.Candidate;
-import com.infy.service.CandidateService;
+import com.infy.service.CandidateServiceImpl;
 
 
 
@@ -25,9 +25,9 @@ public class CandidateTester {
 		
 		String message = null;
 		try {
-			CandidateService candidateService = new CandidateService();
-			LocalDate examDate = LocalDate.of(2014, 5, 29);
-			Candidate candidate = new Candidate(12346, "Sam", 51, 56, 78, 'P', "ECE", examDate);
+			CandidateServiceImpl candidateService = new CandidateServiceImpl();
+			LocalDate examDate = LocalDate.of(2014, 5, 23);
+			Candidate candidate = new Candidate(12345, "Sam", 51, 56, 78, 'P', "ECE", examDate);
 			message = candidateService.addCandidate(candidate);
 			System.out.println(message);
 		} catch (Exception  e) {
@@ -37,7 +37,7 @@ public class CandidateTester {
 
 	public static void getGradesForAllCandidates() throws Exception{
 		try {
-		CandidateService candidateService = new CandidateService();
+		CandidateServiceImpl candidateService = new CandidateServiceImpl();
 		String[] allCandidates = candidateService.getGradesForAllCandidates();
 		System.out.println("\tCandidate ID\t\tResult");
 		System.out.println("\t************\t\t******");
--- Infy_Academy/src/main/java/com/infy/validator/Validator.java
+++ Infy_Academy/src/main/java/com/infy/validator/Validator.java
@@ -1,6 +1,78 @@
 package com.infy.validator;
 
+import java.time.LocalDate;
+
+import com.infy.model.Candidate;
+
 public class Validator {
-//	Not Implemented yet
-//	Coming real soon!! 
+	//calls the validation methods for individual inputs
+	//if any method returns false, then the String value correspondingly
+	public String validate(Candidate candidate) {
+		if( !this.isValidCandidateId( candidate.getCandidateId() ) ) {
+			return "The entered candidate ID is invalid.";
+		} else if( !this.isValidCandidateName( candidate.getCandidateName() ) ) {
+			return "The entered candidate name is invalid.";
+		} else if( !this.isValidExamMarks( candidate ) ) {
+			return "The entered exam marks are invalid.";
+		} else if( !this.isValidResult( candidate.getResult() ) ) {
+			return "The entered result is invalid.";
+		} else if( !this.isValidDepartment( candidate.getDepartment() ) ) {
+			return "The entered Department name is invalid.";
+		} else if( !this.isValidExamDate( candidate.getExamDate() ) ) {
+			return "The entered Exam Date is invalid.";
+		}
+		return "Successfully Validated";
+	}	
+
+	// The entered candidate name should contain only alphabets. Cannot have
+	// special characters and only spaces
+	public Boolean isValidCandidateName(String candidateName) {
+		if( candidateName.matches("[A-Za-z]+") ) {
+			return true;
+		}
+		return false;
+	}
+
+	// The entered candidate ID should be of size 5
+	public Boolean isValidCandidateId(Integer candidateId) {
+		if( candidateId.toString().matches("\\d{5}") ) {
+			return true;
+		}
+		return false;
+	}
+
+	// The entered Department name should be one among the given departments
+	// (ECE, CSE, IT, EEE)
+	public Boolean isValidDepartment(String department) {
+		if( department.equals("ECE") || department.equals("CSE")
+			|| department.equals("IT") || department.equals("EEE") ) {
+			return true;
+		}
+		return false;
+	}
+
+	// exam date cannot be today or after todays date
+	public Boolean isValidExamDate(LocalDate examDate) {
+		if( LocalDate.now().compareTo(examDate) >= 0 ) {
+			return true;
+		}  
+		return false;
+	}
+	
+	//Checking if marks are not equal to "0"
+	public Boolean isValidExamMarks(Candidate candidateTO) {
+		if( candidateTO.getMark1() > 0 && candidateTO.getMark2() > 0
+			&& candidateTO.getMark3() > 0)  {
+			return true;
+		}
+		return false;
+	}
+	
+	//Checking if result set is either 'P' or 'F' only
+	public Boolean isValidResult(Character result) {
+		if( result.equals('P') || result.equals('F')) {
+			return true;
+		}
+		return false;
+	}
 }
